b831e5f38b24bf037d83ac0aeb454a9b5dbf3a75
yiyang-fairy <1196342044@qq.com>
2024-07-26 20:10:59 +0800

    js currying

---
+++ JS/手写代码题/17-柯里化函数.js
@@ -0,0 +1,20 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...nextArgs) {
+        return curried.apply(this, args.concat(nextArgs));
+      };
+    }
+  };
+}
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+const curriedSum = curry(sum);
+console.log(curriedSum(1)(2)(3)); // 输出: 6
+console.log(curriedSum(1, 2)(3)); // 输出: 6
+console.log(curriedSum(1)(2, 3)); // 输出: 6
