9d16e2c789bce3492f7109cc950b933eb5156082
yiyang-fairy <1196342044@qq.com>
2024-11-05 11:18:14 +0800

    创建普通元素,创建普通文本

--- react/mini-react/main.js
+++ react/mini-react/main.js
@@ -1,20 +1,29 @@
-const textEl = {
-  type: "TEXT_ELEMENT",
-  props: {
-    nodeValue: "app",
-    children: [],
-  },
+const createTextNode = (text, ...children) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: children,
+    },
+  }
 }
-const el = {
-  type: "div",
-  props: {
-    id: "app",
-    children: [textEl],
-  },
+
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: typeof children === "string" ? createTextNode(children) : children,
+    },
+  }
 }
-const dom = document.createElement(el.type)
-dom.id = el.props.id
+
+const textE1 = createTextNode("app2")
+const App = createElement("div", { id: "app" }, textE1)
+
+const dom = document.createElement(App.type)
+dom.id = App.props.id
 document.querySelector("#root").append(dom)
 const textNode = document.createTextNode("")
-textNode.nodeValue = textEl.props.nodeValue
+textNode.nodeValue = textE1.props.nodeValue
 dom.appendChild(textNode)
