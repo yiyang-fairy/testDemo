91f4cc9ee88a30bd1d638d38c6c4aaa7092e5d3b
yiyang-fairy <1196342044@qq.com>
2024-11-12 14:52:39 +0800

    任务调度器和fiber架构

--- react/mini-react/miniReact/App.jsx
+++ react/mini-react/miniReact/App.jsx
@@ -1,8 +1,8 @@
 import React from "./core/React.js"
 
-// const App = React.createElement("div", { id: "app" }, "hi-", "mini-react")
+const App = React.createElement("div", { id: "app" }, "hi-", "mini-react")
 
-const App = <div id="app">hi-mini-react</div>
+// const App = <div id="app">hi-mini-react</div>
 
 console.log(App)
 export default App
--- react/mini-react/miniReact/core/React.js
+++ react/mini-react/miniReact/core/React.js
@@ -21,21 +21,82 @@ const createElement = (type, props, ...children) => {
 }
 
 const render = (element, container) => {
-  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type)
-
-  const props = element.props || {}
-  
-  Object.keys(props).filter(item => item !== "children").forEach(name => {
-    dom[name] = props[name]
-  })
-
-  const children = props.children || []
-  children.forEach(child => {
-    render(child, dom)
-  })
-
-  container.appendChild(dom)
+  nextWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
 }
+
+let nextWork = null
+const workLoop = (deadline) => {
+  let shouldYield = false
+  while (!shouldYield && nextWork) {
+    nextWork = performWorkOfUnit(nextWork)
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+const createDom = (type) =>  {
+  return type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(type)
+}
+
+const updateProps = (dom, props) => {
+  Object.keys(props).forEach(key => {
+    if (key !== "children") {
+      dom[key] = props[key]
+    }
+  })
+}
+
+const initChildren = (fiber) => {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+
+const performWorkOfUnit = (fiber) => {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+
+    fiber.parent.dom.append(dom)
+
+    updateProps(dom, fiber.props)
+  }
+
+  initChildren(fiber)
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+
+  return fiber.parent?.sibling
+}
+
+requestIdleCallback(workLoop)
 
 const React = {
   createElement,
